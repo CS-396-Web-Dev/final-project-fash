dcafd2a8cab53a9a1c30615c2898c7f3416861ac
Dragoon788 <109482739+Dragoon788@users.noreply.github.com>
2024-11-25 14:21:47 -0600

    Added state variables

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ import './style.css';
 
 export default function Home() {
   // add states
+  const [health, setHealth] = useState(100);
+  const [hunger, setHunger] = useState(100);
+  const [iq, setIQ] = useState(100);
+  const [motivation, setMotiviation] = useState(100);
 
   // return HTML
 }
