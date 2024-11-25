230af01e40f6c8dba9d248c8368df06d8381ed9e
hein-kim <heinkim1001@gmail.com>
2024-11-25 14:14:13 -0600

    Update page.tsx
    cleaning starter code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
 'use client';
-import Image from 'next/image';
-import RecipeStep from './RecipeStep';
-import Layout from './Layout';
-import Card from './Card';
 import { useEffect, useState } from 'react';
 import './style.css';
 
