1baf297ebe3acb9b8bef9bdce5af8dcc29987971
apcurran21 <102324978+apcurran21@users.noreply.github.com>
2024-11-25 15:31:22 -0600

    revised MotivationContext structure

--- app/contexts/MotivationContext.tsx
+++ app/contexts/MotivationContext.tsx
@@ -11,5 +11,25 @@ import {
   }
 
   interface Motivation {
-    // motivation: int
+    motivation: number;
+    setMotivation: Dispatch<any>;
+  }
+
+  const MotivationContext = createContext<Motivation>({
+    motivation: 0,
+    setMotivation: () => {},
+  });
+
+  export const useMotivationContext = () => useContext(MotivationContext);
+
+  export default function MotivationContextProvider({
+    children,
+  }: MotivationContextProviderProps) {
+    const [motivation, setMotivation] = useState(0);
+
+    return (
+        <MotivationContext.Provider value={{ motivation, setMotivation }}>
+            {children}
+        </MotivationContext.Provider>
+    );
   }
