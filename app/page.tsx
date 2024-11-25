a202fb789b2ea712bf38752987e7b3740eea84ed
hein-kim <heinkim1001@gmail.com>
2024-11-25 14:38:03 -0600

    Update page.tsx
    saving and loading states from local storage

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,74 @@ export default function Home() {
   // add states
   const [health, setHealth] = useState(100);
   const [hunger, setHunger] = useState(100);
-  const [iq, setIQ] = useState(100);
-  const [motivation, setMotiviation] = useState(100);
+  const [iq, setIq] = useState(100);
+  const [motivation, setMotivation] = useState(100);
+
+  //
+  // // LOCAL STORAGE: STATES
+  // 
+
+  // // HEALTH
+
+  // load health state from localStorage when home page mounts
+  useEffect(() => {
+    const savedHealth = localStorage.getItem('health');
+    if (savedHealth) {
+      setHealth(JSON.parse(savedHealth)); // set health state to stored health
+    }
+  }, []);
+
+  // save health state to localStorage, whenever health state changes
+  useEffect(() => {
+    localStorage.setItem('health', JSON.stringify(health)); // save the health state locally
+  }, [health]);
+
+  // // HUNGER
+  
+  // load hunger state from localStorage when home page mounts
+  useEffect(() => {
+    const savedHunger = localStorage.getItem('hunger');
+    if (savedHunger) {
+      setHunger(JSON.parse(savedHunger)); // set hunger state to stored hunger
+    }
+  }, []);
+
+  // save hunger state to localStorage, whenever hunger state changes
+  useEffect(() => {
+    localStorage.setItem('hunger', JSON.stringify(hunger)); // save the hunger state locally
+  }, [hunger]);
+
+  // // IQ
+  
+  // load IQ state from localStorage when home page mounts
+  useEffect(() => {
+    const savedIQ = localStorage.getItem('iq');
+    if (savedIQ) {
+      setIq(JSON.parse(savedIQ)); // set IQ state to stored IQ
+    }
+  }, []);
+
+  // save IQ state to localStorage, whenever IQ state changes
+  useEffect(() => {
+    localStorage.setItem('iq', JSON.stringify(iq)); // save the IQ state locally
+  }, [iq]);
+
+
+  // // MOTIVATION
+  
+  // load motivation state from localStorage when home page mounts
+  useEffect(() => {
+    const savedMotivation = localStorage.getItem('motivation');
+    if (savedMotivation) {
+      setMotivation(JSON.parse(savedMotivation)); // set IQ state to stored IQ
+    }
+  }, []);
+
+  // save IQ state to localStorage, whenever IQ state changes
+  useEffect(() => {
+    localStorage.setItem('motivation', JSON.stringify(motivation)); // save the IQ state locally
+  }, [motivation]);
 
   // return HTML
+  return ();
 }
