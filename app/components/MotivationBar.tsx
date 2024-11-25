4f8440015e8d2088c3dad78e73d29168a0262c95
apcurran21 <102324978+apcurran21@users.noreply.github.com>
2024-11-25 16:55:32 -0600

    fixed tailwind code and switched appearance of Motivation bar

--- app/components/MotivationBar.tsx
+++ app/components/MotivationBar.tsx
@@ -5,13 +5,17 @@ export default function MotivationBar() {
     // get current motivation level from the context
     const { motivation } = useMotivationContext();
 
+    console.log("Motivation has value ", motivation);
+
     return (
         // tailwind status bar from https://flowbite.com/docs/components/progress/
-        <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-            <div 
-                className="bg-blue-600 h-2.5 rounded-full"
-                style={{ width: `${motivation}%` }}
-            ></div> 
-        </div>
+            // <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+            <div className="w-1/2 bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+                <div 
+                    className="bg-blue-600 h-2.5 rounded-full"
+                    // style="width: 50%"
+                    style={{ width: `${motivation}%` }}
+                ></div> 
+            </div>
     );
 }
