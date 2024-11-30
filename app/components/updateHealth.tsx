7307948b20f3c27f3d991d2150f4d8ccf88c5902
hein-kim <heinkim1001@gmail.com>
2024-11-30 14:12:24 -0600

    tied button components to corresponding displayed text state

--- app/components/updateHealth.tsx
+++ app/components/updateHealth.tsx
@@ -1,5 +1,6 @@
 import { useHealthContext } from '@/app/contexts/HealthContext';
 import { useEffect } from 'react';
+import { useTextContext } from '../contexts/TextContext';
 
 // var counter: number = 0;
 
@@ -11,16 +12,23 @@ interface HealthProps {
 
 export default function UpdateHealth({ updateValue, buttonName }: HealthProps) {
     const { setHealth, health } = useHealthContext();
+    const { text, setText } = useTextContext();
 
   // Function to update the health by passed in value
   const onUpdateHealth = () => {
     setHealth(Math.min(Math.max(health + updateValue, 0), 100));
+    if (buttonName == 'Exercise') {
+      setText('You did a Chloe Ting workout with Chicken. Health went up 5 points.');
+    } else {
+      setText('You made Chicken eat dining hall food. Health went down 5 points.')
+    }
   };
 
     // Log whenever health changes
     useEffect(() => {
     console.log(health);
-    }, [health]);
+    console.log(text);
+    }, [health, text]);
 
   return (
     <>
