0c174617da0efce57eb91dc3119bf559ff3a73c7
Dragoon788 <109482739+Dragoon788@users.noreply.github.com>
2024-11-30 13:17:47 -0600

    Fix Intermediary Characters

--- app/components/DisplayCharacter.tsx
+++ app/components/DisplayCharacter.tsx
@@ -8,12 +8,28 @@ export default function displayCharacter() {
   const { health, setHealth} = useHealthContext();
 
   var tempHealth: number = health;
+  var tempIQ: number = IQ;
+
+  if (IQ < 200 && IQ > 150){
+    tempIQ = 200;
+  }
+  if (IQ < 150 && IQ > 100){
+    tempIQ = 150;
+  }
+  if (IQ < 100 && IQ > 50){
+    tempIQ = 100;
+  }
+  if (IQ < 50 && IQ > 0){
+    tempIQ = 50;
+  }
   if (health < 100 && health > 50){
     tempHealth = 100;
   }
   if (health < 50 && health > 0){
     tempHealth = 50;
   }
+
+
 
 //   // Function to update the health by passed in value
 //   const determineImage = () => {
@@ -28,7 +44,7 @@ export default function displayCharacter() {
   return (
     <div className="inline-block">
         <img
-          src={`character_health${tempHealth}_iq${IQ}.png`}
+          src={`character_health${tempHealth}_iq${tempIQ}.png`}
           alt="my image"
           className="w-20 h-20"
         />
